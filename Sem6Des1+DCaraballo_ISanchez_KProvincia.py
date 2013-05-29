098ad269dc576912ece62f6cc0d8e81041e4e449
hybrydo <hybrydo@gmail.com>
2013-05-29 02:01:32 +0200

    Creando archivo base para el desafio

---
+++ Sem6Des1+DCaraballo_ISanchez_KProvincia.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#Semana 6 - Desafío 1
+#Autores:    +Diego Caraballo +Ibán Sánchez Villanueva +Kelvin Provincia  
+#Fecha:      29/05/13
+
+
+#Especificaciones,
+#Desarrollar un programa que permita crear diseños a partir de patrones y desplazamientos:
+    #Los patrones pueden ser cualquier cadena de caracteres de una misma longitud y son ingresados por teclado.
+    #Los patrones forman capas que se superponen. Las de nivel superior se hubican encima de las de un nivel inferior.
+    #Para cada capa o patrón se define un desplazamiento que puede ser nulo, 1 o -1.
